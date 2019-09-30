0a5ad34a86860ff1198b1e6eef7643cad92be946
Innocent Mwatsikesimbe <31913074+imwatsi@users.noreply.github.com>
2019-09-30 12:59:26 +0200

    add username generator script

---
+++ username_gen/username_gen.py
@@ -0,0 +1,26 @@
+import random
+import re
+
+regex = re.compile('[^a-zA-Z]') # to remove non-letter characters
+
+# create list of all keywords from text file
+f = open("keywords.txt", "r").read()
+_keywords = f.split("\n")
+keywords = []
+for k in _keywords:
+    sample = regex.sub('', k)
+    if sample != '':
+        keywords.append(sample)
+
+# generate unique username
+total_keywords = len(keywords)
+_username = []
+while True:
+    x = random.randint(0,total_keywords)
+    if keywords[x] not in _username:
+        _username.append(keywords[x])
+    if len(_username) == 2:
+        break
+
+username = "".join(_username)
+print(username)
